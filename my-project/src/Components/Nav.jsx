1e7409883b62f4fd23a03c6814e7277b147c9068
shawon-1998 <billalhossainshawon989@gmail.com>
2026-02-05 01:52:06 +0600

    toggle and done

--- my-project/src/Components/Nav.jsx
+++ my-project/src/Components/Nav.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from './Container'
 import Flex from './Flex'
+import { FaBars } from "react-icons/fa";
 
 
 const Nav = () => {
+
+  const [value,setValue]=useState(true)
+  
+  const handleClick=()=>{
+    setValue(!value)
+  }
+
   return (
     <>
-      <nav className='bg-transparent py-5 '>
+      <nav className='bg-transparent py-5 lg:px-0 px-10 '>
         <Container>
-         <Flex className=''>
+          <Flex className='justify-between lg:items-center items-start relative'>
             <h3 className='text-transparent text-3xl font-semibold uppercase bg-[url(assets/sunset-2180346.jpg)] bg-no-repeat bg-cover bg-center bg-clip-text anime font-primary'>Shawon</h3>
-          <ul className=' text-secondary text-2xl flex font-primary gap-10'>
-            <li><a href="">Home</a></li>
-            <li ><a href="#AboutMe">About</a></li>
-            <li><a href="#Skill">Skill</a></li>
-            <li><a href="#project">Project</a></li>
-            <li><a href="#Contact">Contact</a></li>
-          </ul>
-         </Flex>
+           
+            <div className={`${value ? "lg:block hidden" :"lg:hidden block"}`}>
+              <ul className='transition-all 0.1s ease-in-out text-secondary text-2xl lg:flex font-primary gap-10'>
+                <li><a href="">Home</a></li>
+                <li><a href="#AboutMe">About</a></li>
+                <li><a href="#Skill">Skill</a></li>
+                <li><a href="#project">Project</a></li>
+                <li><a href="#Contact">Contact</a></li>
+              </ul>
+            </div>
+
+            <FaBars className='lg:hidden block cursor-pointer' onClick={handleClick}/>
+
+          </Flex>
         </Container>
       </nav>
     </>
