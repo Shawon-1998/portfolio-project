04d1f0ea01cd0bcd14855aa254146fc0c3446844
shawon-1998 <billalhossainshawon989@gmail.com>
2026-02-05 16:07:13 +0600

    resizing

--- my-project/src/Components/Nav.jsx
+++ my-project/src/Components/Nav.jsx
@@ -19,8 +19,8 @@ const Nav = () => {
           <Flex className='justify-between lg:items-center items-start relative'>
             <h3 className='text-transparent text-3xl font-semibold uppercase bg-[url(assets/sunset-2180346.jpg)] bg-no-repeat bg-cover bg-center bg-clip-text anime font-primary'>Shawon</h3>
            
-            <div className={`${value ? "lg:block hidden" :"lg:hidden block"}`}>
-              <ul className='transition-all 0.1s ease-in-out text-secondary text-2xl lg:flex font-primary gap-10'>
+            <div className={`${value ? "lg:block hidden " :"lg:hidden block"}`}>
+              <ul className='list transition-all 0.7s lg:static absolute top-0 right-5 ease-in-out mt-5 text-secondary lg:text-2xl text-xl lg:flex font-primary gap-10'>
                 <li><a href="">Home</a></li>
                 <li><a href="#AboutMe">About</a></li>
                 <li><a href="#Skill">Skill</a></li>
@@ -29,7 +29,7 @@ const Nav = () => {
               </ul>
             </div>
 
-            <FaBars className='lg:hidden block cursor-pointer' onClick={handleClick}/>
+            <FaBars className='lg:hidden block cursor-pointer text-lg mt-3' onClick={handleClick}/>
 
           </Flex>
         </Container>
