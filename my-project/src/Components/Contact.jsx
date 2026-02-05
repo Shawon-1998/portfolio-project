e16ee9414c552879b3e76943a45714a3b9527bbf
shawon-1998 <billalhossainshawon989@gmail.com>
2026-02-06 00:00:07 +0600

    added

--- my-project/src/Components/Contact.jsx
+++ my-project/src/Components/Contact.jsx
@@ -18,15 +18,15 @@ const Contact = () => {
             <form className='lg:w-[60%] w-full mt-10' action="">
                <div className='lg:flex justify-between '>
                  <label htmlFor="text"></label>
-                <input type="text" className='text-primary lg:w-[50%] w-full lg:me-5  transition 0.3s hover:scale-105 lg:py-5 py-3 ps-6 lg:ps-10 rounded-full bg-secondary border outline-0 placeholder:text-primary mb-5 lg:mb-0placeholder:text-lg' placeholder='Insert your name'/>
+                <input type="text" className='text-primary lg:w-[50%] w-full lg:me-5  transition 0.3s hover:scale-105 lg:py-5 py-3 ps-6 lg:ps-10 rounded-full bg-secondary border outline-0 placeholder:text-primary  lg:mb-0placeholder:text-lg' placeholder='Insert your name'/>
                 <label htmlFor="email"></label>
-                <input type="email" className='text-primary lg:w-[50%] w-full lg:mt-0   transition 0.3s hover:scale-105  lg:py-5 py-3 ps-6 lg:ps-10  rounded-full bg-secondary border outline-0 placeholder:text-primary placeholder:text-lg' placeholder='Insert your email'/>
+                <input type="email" className='text-primary mt-5 lg:w-[50%] w-full lg:mt-0   transition 0.3s hover:scale-105  lg:py-5 py-3 ps-6 lg:ps-10  rounded-full bg-secondary border outline-0 placeholder:text-primary placeholder:text-lg' placeholder='Insert your email'/>
                </div>
                 <label htmlFor="text"></label>
                 <input type="" className='text-primary lg:my-7 my-5 w-full  transition 0.3s hover:scale-105  lg:py-5 py-3 ps-6 lg:ps-10 rounded-full bg-secondary border outline-0 placeholder:text-primary placeholder:text-lg' placeholder='Insert your subject'/>
 
                 <label htmlFor="text"></label>
-                <input type="text" className='text-primary transition 0.3s w-full hover:scale-105  lg:py-5 py-3 ps-6 lg:ps-10 pb-25  rounded-4xl bg-secondary border outline-0 placeholder:text-primary placeholder:text-lg' placeholder='Write your message'/>
+                <input type="text" className='text-primary transition 0.3s w-full hover:scale-105   py-3 ps-6 lg:ps-10 pb-25  rounded-4xl bg-secondary border outline-0 placeholder:text-primary placeholder:text-lg' placeholder='Write your message'/>
             </form>
         </div>
       </div>
--- my-project/src/Components/Nav.jsx
+++ my-project/src/Components/Nav.jsx
@@ -18,9 +18,9 @@ const Nav = () => {
         <Container>
           <Flex className='justify-between lg:items-center items-start'>
             <h3 className='text-transparent text-3xl font-semibold uppercase bg-[url(assets/sunset-2180346.jpg)] bg-no-repeat bg-cover bg-center bg-clip-text anime font-primary'>Shawon</h3>
-           
-            <div className={`${value ? "lg:block hidden":"lg:hidden block"} relative`}>
-              <ul className='list transition-all 0.7s lg:static  absolute top-0 right-5 ease-in-out mt-5 text-secondary lg:text-2xl text-xl lg:flex font-primary gap-10'>
+            
+            <div className='relative'>
+              <ul className={`${value ? "lg:block hidden":"lg:hidden block"}  list transition-all 0.7s lg:static absolute z-50 top-0 right-5 ease-in-out mt-5 text-secondary lg:text-2xl text-xl lg:flex font-primary gap-10 `}>
                 <li><a href="#Home">Home</a></li>
                 <li><a href="#AboutMe">About</a></li>
                 <li><a href="#Skill">Skill</a></li>
@@ -28,9 +28,7 @@ const Nav = () => {
                 <li><a href="#Contact">Contact</a></li>
               </ul>
             </div>
-
-            <FaBars className='lg:hidden block cursor-pointer text-lg mt-3' onClick={handleClick}/>
-
+            <FaBars className='lg:hidden block cursor-pointer text-lg mt-3 ' onClick={handleClick}/>
           </Flex>
         </Container>
       </nav>
